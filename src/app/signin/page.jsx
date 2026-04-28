c94705bf0a044fd638a6cb76c35f542a4fc3b355
Mahamudur10 <mahamudur649@gmail.com>
2026-04-29 01:12:20 +0600

    set google credintial

--- src/app/signin/page.jsx
+++ src/app/signin/page.jsx
@@ -11,6 +11,7 @@ import {
     Label,
     TextField,
 } from "@heroui/react";
+import { GrGoogle } from "react-icons/gr";
 
 export default function SignINPage() {
     const onSubmit = async (e) => {
@@ -27,6 +28,11 @@ export default function SignINPage() {
         })
         console.log({ data, error })
     };
+    const handleGoogleSignIn = async () =>{
+        await authClient.signIn.social({
+            provider: 'google'
+        })
+    }
 
     return (
         <Card className="border mx-auto w-125 py-10 mt-5">
@@ -88,6 +94,8 @@ export default function SignINPage() {
                     </Button>
                 </div>
             </Form>
+            <p className="text-center">Or</p>
+            <Button onClick={handleGoogleSignIn} variant="outline" className={'w-full'}><GrGoogle/>Sign In with Google</Button>
         </Card>
     );
 }
