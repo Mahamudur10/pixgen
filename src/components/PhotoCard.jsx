d2223d3c4725491eb13145d331e8f6694bbc838a
Mahamudur10 <mahamudur649@gmail.com>
2026-04-28 16:05:44 +0600

    added top generation and card

---
+++ src/components/PhotoCard.jsx
@@ -0,0 +1,41 @@
+import { Button, Card, Chip, Separator } from '@heroui/react';
+import Image from 'next/image';
+import React from 'react';
+import { BiDownload } from 'react-icons/bi';
+import { FaHeart } from 'react-icons/fa';
+
+const PhotoCard = ({ photo }) => {
+    console.log(photo)
+    return (
+        <Card className='border rounded-xl'>
+            <div className='relative w-full aspect-square'>
+                <Image
+                    src={photo.imageUrl}
+                    fill
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                    alt={photo.title}
+                    className='rounded-xl object-cover'
+                />
+                <Chip size='sm' className='absolute right-1 top-1.5'>{photo.category}</Chip>
+            </div>
+            <div>
+                <h2 className='font-medium'>{photo.title}</h2>
+            </div>
+
+            <div className='flex gap-5'>
+                <div className='flex items-center gap-2'>
+                    <p><FaHeart /></p>
+                    <p>{photo.likes}</p>
+                </div>
+                <Separator orientation='vertical' />
+                <div className='flex items-center gap-2'>
+                    <p><BiDownload /></p>
+                    <p>{photo.downloads}</p>
+                </div>
+            </div>
+            <Button variant='outline' className={'w-full'}>View</Button>
+        </Card>
+    );
+};
+
+export default PhotoCard;
